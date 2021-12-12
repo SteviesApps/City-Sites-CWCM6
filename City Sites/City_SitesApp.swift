76159fcca33d95965bf5f4d1fd43c65d3b09e79c
Steven R. Kaiser <steven@whidbey.com>
2021-12-11 22:04:06 -0800

    Initial Commit

---
+++ City Sites/City_SitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SitesApp.swift
+//  City Sites
+//
+//  Created by Steven Kaiser on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct City_SitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
